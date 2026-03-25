d002888167142415f94fa269c17e85d82e46bbc5
TanBuiDev <tanbui2672@gmail.com>
2026-03-26 00:26:51 +0700

    v1

---
+++ src/main/java/Portfolio/TanBuiDev/TanBuiDevApplication.java
@@ -0,0 +1,13 @@
+package Portfolio.TanBuiDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanBuiDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TanBuiDevApplication.class, args);
+	}
+
+}
